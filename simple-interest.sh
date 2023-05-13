971b8d9107e16b43fc91941f9793f89e93514081
Muhammad Aslam Khan <87271669+muhammad-aslam-khan@users.noreply.github.com>
2023-05-13 15:34:59 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # muhammad-aslam-khan
    # Input:
    # p, principal amount
    # t, time period in years
